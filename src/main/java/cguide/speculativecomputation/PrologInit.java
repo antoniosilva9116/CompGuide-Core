a16aed9b6582dc6c8fff5f90f40b074139999f07
antoniosilva <antoniosilva9116@gmail.com>
2015-06-30 00:00:18 +0100

    first

---
+++ src/main/java/cguide/speculativecomputation/PrologInit.java
@@ -0,0 +1,13 @@
+package cguide.speculativecomputation;
+
+public class PrologInit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
