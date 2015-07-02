324546ab21d834357882e9936ddd903ac34f630f
antoniosilva <antoniosilva9116@gmail.com>
2015-07-02 15:14:29 +0100

    cenas
    asdasd

--- src/main/java/cguide/execution/webservices/GuidelinesWebService.java
+++
@@ -1,8 +0,0 @@
-package cguide.execution.webservices;
-
-/**
- * Created with IntelliJ IDEA. User: tiago Date: 31-08-2013 Time: 14:25 To
- * change this template use File | Settings | File Templates.
- */
-public class GuidelinesWebService {
-}
